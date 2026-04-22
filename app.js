6814e06eca793dbc30e34f629088ef9f6ea116d0
Developer <developer@smartqueue.local>
2026-04-22 21:45:05 +0530

    check app.js

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ import serviceRoutes from './routes/serviceRoutes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:process.env.ALLOW_ORIGINS || 'http://localhost:3000',
+  credentials: true 
+}))
 app.use(express.json());
 app.use(cookieParser());
 
