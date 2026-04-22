0580e6e4ef2e56ea3a971e7c3e25f3ef611a8f0b
Developer <developer@smartqueue.local>
2026-04-22 22:41:54 +0530

    check app.js4

--- app.js
+++ app.js
@@ -10,31 +10,31 @@ import serviceRoutes from './routes/serviceRoutes.js';
 
 const app = express();
 
-const allowedOrigins = process.env.ALLOW_ORIGINS 
-  ? process.env.ALLOW_ORIGINS.split(',').map(o => o.trim())
-  : ['https://queue-beryl-six.vercel.app', 'http://localhost:5173', 'http://localhost:5174'];
+const allowedOrigins = process.env.ALLOW_ORIGINS
+    ? process.env.ALLOW_ORIGINS.split(',').map(o => o.trim())
+    : ['https://queue-backend-six.vercel.app/', 'http://localhost:5173', 'http://localhost:5174'];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true 
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
 }))
 app.use(express.json());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-  res.send('Working...');
+    res.send('Working...');
 });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/user', userRoutes); 
-app.use('/api/services', serviceRoutes); 
+app.use('/api/user', userRoutes);
+app.use('/api/services', serviceRoutes);
 app.use('/api/queue', queueRoutes);
 
 export default app;
